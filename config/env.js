8bff1b2d31f5b1602e3778b23ccaf66f4160ee29
Andrey Antifeev <andrgit@mail.ru>
2021-11-25 02:46:23 +0300

    first good runs (serve, test:unit)

---
+++ config/env.js
@@ -0,0 +1,5 @@
+module.exports = {
+    host: 'localhost',
+    publicPath: 'http://localhost:8080/',
+    port: 8080,
+};
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,26 @@
-module.exports = {};
+const path = require('path');
+const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+
+const _envJS = path.resolve(__dirname, './config/env.js');
+const _packageJSONPath = path.resolve(__dirname, './package.json');
+const _packageJSON = require(_packageJSONPath);
+
+module.exports = {
+    publicPath: _envJS.publicPath,
+    configureWebpack: {
+        plugins: [
+            new ModuleFederationPlugin({
+                name: _packageJSON.name,
+                filename: "remoteEntry.js",
+            //     // exposes: {},
+            //     // remotes: {},
+            //     // shared: {},
+            }),
+        ],
+    },
+
+    devServer: {
+        host: _envJS.host,
+        port: _envJS.port,
+    }
+};
